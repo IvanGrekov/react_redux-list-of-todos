95fd987af254510d37046474e3ea86cf1d582d89
Ivan Grekov <grekovv76@gmail.com>
2021-07-01 20:30:07 +0300

    Updates

---
+++ precache-manifest.b318452be288e941dd3895095c1d2cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6a944fc32efbe1bb19f1a2dbfd837a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d9261ded16e19a333b2",
+    "url": "./static/css/main.46309652.chunk.css"
+  },
+  {
+    "revision": "52a34b8cfd88c6759110",
+    "url": "./static/js/2.3177e2db.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.3177e2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9261ded16e19a333b2",
+    "url": "./static/js/main.a4294dbf.chunk.js"
+  },
+  {
+    "revision": "6e2c239f7a333241bc85",
+    "url": "./static/js/runtime-main.cb0ead06.js"
+  }
+]);
